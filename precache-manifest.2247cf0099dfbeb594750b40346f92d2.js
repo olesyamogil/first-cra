02a21842149110375ecfe3e4bff77840fccba80b
olesyamogil <olesya.mogil@gmail.com>
2019-10-25 13:38:36 +0300

    Updates

---
+++ precache-manifest.2247cf0099dfbeb594750b40346f92d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87cfc25fecd5022094d3c8786d56667",
+    "url": "/first-cra/index.html"
+  },
+  {
+    "revision": "8ec7be4a00daa9e9ff26",
+    "url": "/first-cra/static/css/main.97ff4449.chunk.css"
+  },
+  {
+    "revision": "fdfd8a5493c50979c8b9",
+    "url": "/first-cra/static/js/2.3cae48ed.chunk.js"
+  },
+  {
+    "revision": "8ec7be4a00daa9e9ff26",
+    "url": "/first-cra/static/js/main.f894a7b7.chunk.js"
+  },
+  {
+    "revision": "01918f9776c8ac7ba288",
+    "url": "/first-cra/static/js/runtime-main.754abfd1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/first-cra/precache-manifest.dca0acc9596cc1169a1bb2811f866bd5.js"
+  "/first-cra/precache-manifest.2247cf0099dfbeb594750b40346f92d2.js"
 );
 
 self.addEventListener('message', (event) => {
