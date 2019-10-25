5680bc7fc0686f1fcf1ecf40a819e27a795336ce
olesyamogil <olesya.mogil@gmail.com>
2019-10-25 13:00:25 +0300

    Updates

---
+++ precache-manifest.32c179bd6e2fed6bd78fd218a71e86ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615216400e2cfb992aa5db14e42610fa",
+    "url": "/first-cra/index.html"
+  },
+  {
+    "revision": "a4b308120d176ee0dc00",
+    "url": "/first-cra/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c652b7d320b82a4c7421",
+    "url": "/first-cra/static/js/2.014af5fd.chunk.js"
+  },
+  {
+    "revision": "a4b308120d176ee0dc00",
+    "url": "/first-cra/static/js/main.a15d9aa7.chunk.js"
+  },
+  {
+    "revision": "01918f9776c8ac7ba288",
+    "url": "/first-cra/static/js/runtime-main.754abfd1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/first-cra/static/media/logo.25bf045c.svg"
+  }
+]);
